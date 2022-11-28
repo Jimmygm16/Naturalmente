e0b5de4b9972bdde4f913dcdd3090fa106533740
PabloGoProg <pgosorio13@gmail.com>
2022-11-28 14:18:32 -0500

    Repo init

---
+++ src/main/java/com/naturalmente/Naturalmente/NaturalmenteApplication.java
@@ -0,0 +1,13 @@
+package com.naturalmente.Naturalmente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalmenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalmenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naturalmente/Naturalmente/NaturalmenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naturalmente.Naturalmente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalmenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
